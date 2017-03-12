5c518809ba847934621127f6e3f2076733156858
Matthew McGrattan <matthew.mcgrattan@bodleian.ox.ac.uk>
2017-03-12 22:54:17 +0000

    pep8 and minor changes in anno reformatting

--- base_proxy.py
+++ base_proxy.py
@@ -1,5 +1,6 @@
 import flask
 import requests
+import json
 
 
 def repair_results(json_dict):
@@ -15,6 +16,8 @@ def repair_results(json_dict):
 
     oa:Tag
     """
+    anno_list = {"@context": "http://iiif.io/api/presentation/2/context.json", "@type": "sc:AnnotationList",
+                 'resources': []}
     for item in json_dict:
         resource = item['resource']
         for res in resource:
@@ -24,7 +27,8 @@ def repair_results(json_dict):
             if 'oa:hasPurpose' in res.keys():
                 del res['oa:hasPurpose']
                 res['@type'] = 'oa:Tag'
-    return json.dumps(json_dict, indent=4)
+            anno_list['resources'].append(res)
+    return json.dumps(anno_list, indent=4)
 
 
 def got_body(json_data):
@@ -44,13 +48,15 @@ def got_body(json_data):
 
 app = flask.Flask(__name__)
 
+
 @app.route('/annotationlist/<path:anno_container>', methods=['GET'])
 def brilleaux(anno_container):
     if flask.request.method == 'GET':
         anno_server = 'https://annotation-dev.digtest.co.uk:443/annotation/w3c/'
         request_uri = ''.join([anno_server, anno_container])
         try:
-            r = requests.get(request_uri,headers={'Accept': 'Application/ld+json; profile="http://iiif.io/api/presentation/2/context.json"'})
+            r = requests.get(request_uri, headers={
+                'Accept': 'Application/ld+json; profile="http://iiif.io/api/presentation/2/context.json"'})
             if r.status_code == requests.codes.ok:
                 try:
                     content = got_body(r.json())
