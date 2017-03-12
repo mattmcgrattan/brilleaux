5105f514e07479e6e42a528059fdbcaadba093f3
Matthew McGrattan <matthew.mcgrattan@bodleian.ox.ac.uk>
2017-03-12 22:50:16 +0000

    Simplified and inlined a couple of functions.

--- base_proxy.py
+++ base_proxy.py
@@ -1,6 +1,46 @@
 import flask
 import requests
-import elucidate
+
+
+def repair_results(json_dict):
+    """
+    Takes a result returned from Digirati
+    Annotation Server, which does NOT
+    display properly using the SimpleAnnotation
+    endpoint in Mirador, and makes:
+
+    value = chars
+
+    and turns all oa:hasPurpose into:
+
+    oa:Tag
+    """
+    for item in json_dict:
+        resource = item['resource']
+        for res in resource:
+            if 'value' in res.keys():
+                res['chars'] = res['value']
+                del res['value']
+            if 'oa:hasPurpose' in res.keys():
+                del res['oa:hasPurpose']
+                res['@type'] = 'oa:Tag'
+    return json.dumps(json_dict, indent=4)
+
+
+def got_body(json_data):
+    """
+    Checks to see if a paged list is returned.
+
+    If yes, grab the first page in the list,
+    get the content.
+
+    Turn the list of items into an annotation result.
+    """
+    content_dict = json_data
+    anno_results = content_dict['first']['as:items']['@list']
+    updated = repair_results(anno_results)
+    return updated
+
 
 app = flask.Flask(__name__)
 
@@ -13,7 +53,7 @@ def brilleaux(anno_container):
             r = requests.get(request_uri,headers={'Accept': 'Application/ld+json; profile="http://iiif.io/api/presentation/2/context.json"'})
             if r.status_code == requests.codes.ok:
                 try:
-                    content = elucidate.got_body(r.json())
+                    content = got_body(r.json())
                     resp = content
                 except:
                     resp = 'Nothing'
@@ -23,11 +63,6 @@ def brilleaux(anno_container):
             # this should 500 and return something informative.
             print('I do not know what went wrong.')
             resp = 'Nothing'
-            # e.g.
-            # content = None
-            # headers = dest_headers
-            # status_code = "500"
-            # reason = "Application error retrieving annotations."
     else:
         resp = 'Nothing'
     return resp
