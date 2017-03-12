c0b577a79623db681d2d6bab18e897d497751d52
Matthew McGrattan <matthew.mcgrattan@bodleian.ox.ac.uk>
2017-03-12 22:25:33 +0000

    removed header dict call

--- base_proxy.py
+++ base_proxy.py
@@ -5,14 +5,13 @@ app = flask.Flask(__name__)
 
 @app.route('/annotationlist/<path:anno_container>', methods=['GET'])
 def brilleaux(anno_container):
-    request_headers = dict(flask.request.headers)
     if flask.request.method == 'GET':
         anno_server = 'https://annotation-dev.digtest.co.uk:443/annotation/w3c/'
         request_uri = ''.join([anno_server, anno_container])
         try:
             r = requests.get(request_uri,headers={'Accept': 'Application/ld+json; profile="http://iiif.io/api/presentation/2/context.json"'})
             print(r.status_code)
-            resp = (r.json())
+            resp = r.status_code
         except:
             # this should 500 and return something informative.
             print('I do not know what went wrong.')
