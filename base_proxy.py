dba833caf817366a45ff713f9282f2dae4cd6380
Matthew McGrattan <matthew.mcgrattan@bodleian.ox.ac.uk>
2017-03-13 19:00:15 +0000

    Error handling changes throughout, code cleanup.

--- base_proxy.py
+++ base_proxy.py
@@ -46,8 +46,8 @@ def crossdomain(origin=None, methods=None, headers=None,
 
         f.provide_automatic_options = False
         return update_wrapper(wrapped_function, f)
-    return decorator
 
+    return decorator
 
 
 def repair_results(json_dict, request_uri):
@@ -62,23 +62,35 @@ def repair_results(json_dict, request_uri):
     and turns all oa:hasPurpose into:
 
     oa:Tag
+
+    :rtype: string (Serialized JSON)
     """
     anno_list = {"@context": "http://iiif.io/api/presentation/2/context.json", "@type": "sc:AnnotationList",
                  "@id": request_uri,
                  'resources': []}
-    for item in json_dict:
-        resource = item['resource']
-        for res in resource:
-            if 'value' in res.keys():
-                res['chars'] = res['value']
-                del res['value']
-            if 'oa:hasPurpose' in res.keys():
-                del res['oa:hasPurpose']
-                res['@type'] = 'oa:Tag'
-        o_list = [target_extract(o) for o in item['on']]
-        item['on'] = o_list[0]
-        anno_list['resources'].append(item)
-    return json.dumps(anno_list, indent=4)
+    if len(json_dict) > 0:
+        for item in json_dict:
+            if 'resource' in item:
+                resource = item['resource']
+                for res in resource:
+                    if 'value' in res.keys():
+                        # IIIF Annotations use chars,
+                        # not value.
+                        res['chars'] = res['value']
+                        del res['value']
+                    if 'oa:hasPurpose' in res.keys():
+                        # IIIF Annotations don't use Purpose
+                        del res['oa:hasPurpose']
+                        res['@type'] = 'oa:Tag'
+                if isinstance(item['on'], dict):
+                    item['on'] = target_extract(item['on'])  # o
+                else:
+                    # o_list = [target_extract(o) for o in item['on']]
+                    item['on'] = [target_extract(o) for o in item['on']][0]  # o_list[0]
+                anno_list['resources'].append(item)
+                return json.dumps(anno_list, indent=4)
+    else:
+        return None
 
 
 def target_extract(json_dict):
@@ -100,9 +112,16 @@ def got_body(json_data, request_uri):
     Turn the list of items into an annotation result.
     """
     content_dict = json_data
-    anno_results = content_dict['first']['as:items']['@list']
-    updated = repair_results(anno_results, request_uri)
-    return updated  # json.dumps(anno_results[0], indent=2)
+    if ('first' in content_dict and 'as:items' in content_dict['first']
+        and '@list' in content_dict['first']['as:items']):
+        anno_results = content_dict['first']['as:items']['@list']
+        updated = repair_results(anno_results, request_uri)
+        if updated:
+            return updated
+        else:
+            return None
+    else:
+        return None
 
 
 app = flask.Flask(__name__)
@@ -114,22 +133,25 @@ def brilleaux(anno_container):
     if flask.request.method == 'GET':
         anno_server = 'https://elucidate.dlcs-ida.org/annotation/w3c/'
         request_uri = ''.join([anno_server, anno_container])
-        try:
-            r = requests.get(request_uri, headers={
-                'Accept': 'Application/ld+json; profile="http://iiif.io/api/presentation/2/context.json"'})
-            if r.status_code == requests.codes.ok:
-                try:
-                    content = got_body(r.json(), flask.request.url)
+        # make sure URL ends in a /
+        if request_uri[-1] != '/':
+            request_uri += "/"
+            flask.request.url += "/"
+        r = requests.get(request_uri, headers={
+            'Accept': 'Application/ld+json; profile="http://iiif.io/api/presentation/2/context.json"'})
+        print("Elucidate Status Code")
+        print(r.status_code)
+        if r.status_code == requests.codes.ok:
+            if r.json():
+                content = got_body(r.json(), flask.request.url)
+                if content:
                     resp = flask.Response(content, headers={'Content-Type': 'application/ld+json;charset=UTF-8'})
-                    # resp = json.dumps(r.json(), indent=4, sort_keys=True)
-                except:
-                    resp = 'Nothing'
+                    return resp
+                else:
+                    flask.abort(500)
             else:
-                resp = 'Nothing'
-        except:
-            # this should 500 and return something informative.
-            print('I do not know what went wrong.')
-            resp = 'Nothing'
+                flask.abort(404)
+        else:
+            flask.abort(r.status_code)
     else:
-        resp = 'Nothing'
-    return resp
+        flask.abort(405)
