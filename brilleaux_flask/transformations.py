e7be40b46b738f285ac60f513e28adfd6fb79b63
Matt McGrattan <matt.mcgrattan@digirati.com>
2018-10-03 15:44:44 +0100

    Re-added choice.

--- brilleaux_flask/transformations.py
+++ brilleaux_flask/transformations.py
@@ -7,8 +7,9 @@ def remove_keys(d, keys):
     return {k: v for k, v in d.items() if k in (set(d.keys()) - set(keys))}
 
 
-def target_extract(targets: Union[dict, list], target_format: str = "simple",
-                   fake_selector: Optional[str] = None) -> Optional[Union[list, str, dict]]:
+def target_extract(
+    targets: Union[dict, list], target_format: str = "simple", fake_selector: Optional[str] = None
+) -> Optional[Union[list, str, dict]]:
     """
     Extract the target and turn into a simple 'on'
     :param fake_selector: e.g. "xywh=0,0,50,50" to use for selectorless annotations
@@ -41,17 +42,46 @@ def target_extract(targets: Union[dict, list], target_format: str = "simple",
                         selector_val = t["selector"]["value"]
                     else:
                         if fake_selector:  # i.e. whole canvas or whole manifest annotation
-                            selector_val = fake_selector  # use whatever the default fake selector is
+                            selector_val = (
+                                fake_selector
+                            )  # use whatever the default fake selector is
                     if selector_val:
-                        x, y, w, h = [int(a) for a in selector_val.split('=')[1].split(',')]
+                        x, y, w, h = [int(a) for a in selector_val.split("=")[1].split(",")]
                         path = "".join(
-                            ["M", str(x), ",", str(y), "h", str(0.5 * w), "v0", "h", str(0.5 * w), "v", str(0.5 * h),
-                             "v", str(0.5 * h), "h", str(-0.5 * w), "h", str(-0.5 * w), "v", str(-0.5 * h) + "z"]
+                            [
+                                "M",
+                                str(x),
+                                ",",
+                                str(y),
+                                "h",
+                                str(0.5 * w),
+                                "v0",
+                                "h",
+                                str(0.5 * w),
+                                "v",
+                                str(0.5 * h),
+                                "v",
+                                str(0.5 * h),
+                                "h",
+                                str(-0.5 * w),
+                                "h",
+                                str(-0.5 * w),
+                                "v",
+                                str(-0.5 * h) + "z",
+                            ]
                         )
                         with open("svg_template.html", "r") as template_file:
                             template = Template(template_file.read())
-                            on_dict["selector"] = Template.render(template, path_value=path,
-                                                                  uuid_val=str(uuid.uuid4()))
+                            on_dict["selector"] = {
+                                "@type": "oa:Choice",
+                                "default": {"@type": "oa:FragmentSelector", "value": selector_val},
+                                "item": {
+                                    "@type": "oa:SvgSelector",
+                                    "value": Template.render(
+                                        template, path_value=path, uuid_val=str(uuid.uuid4())
+                                    ),
+                                },
+                            }
                     on.append(on_dict)
             return on
     else:
@@ -72,15 +102,16 @@ def transform_annotation(
                 item["body"] = [transform_function(body) for body in item["body"]]
             elif isinstance(item["body"], dict):
                 item["body"] = transform_function(item["body"])
-            item["on"] = target_extract(targets=item["target"],
-                                        target_format="specificresource",
-                                        fake_selector="xywh=0,0,75,75")
+            item["on"] = target_extract(
+                targets=item["target"],
+                target_format="specificresource",
+                fake_selector="xywh=0,0,75,75",
+            )
             item["@id"] = item["id"]
             item["@type"] = "oa:Annotation"
             item["resource"] = item["body"]
             item = remove_keys(
-                d=item,
-                keys=["generator", "label", "target", "creator", "type", "id", "body"],
+                d=item, keys=["generator", "label", "target", "creator", "type", "id", "body"]
             )
             return item
         else:
@@ -99,16 +130,12 @@ def mirador_oa(w3c_body: dict) -> dict:
     """
     new_body = {}
     if "source" in w3c_body.keys():
-        new_body["chars"] = (
-            '<a href="' + w3c_body["source"] + '">' + w3c_body["source"] + "</a>"
-        )
+        new_body["chars"] = '<a href="' + w3c_body["source"] + '">' + w3c_body["source"] + "</a>"
         new_body["format"] = "application/html"
     if "value" in w3c_body.keys():
         new_body["@type"] = "oa:Tag"
         new_body["chars"] = w3c_body["value"]
-    new_body = remove_keys(
-        new_body, ["value", "type", "generator", "source", "purpose"]
-    )
+    new_body = remove_keys(new_body, ["value", "type", "generator", "source", "purpose"])
     return new_body
 
 
